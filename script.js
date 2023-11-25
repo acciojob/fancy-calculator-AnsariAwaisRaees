92346d5efa2dbca07ae63cfb8220799add2ea393
Awais Raees Ansari <80111573+AnsariAwaisRaees@users.noreply.github.com>
2023-11-25 20:21:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let a = prompt("Enter the first value");
+let var1 = parseInt(a) * 2;
+
+let b = prompt("Enter the second value");
+let var2 = parseInt(b);
+
+let res = var1 + var2;
+
+alert("The final result is: "+res);
